bac0e8b01dedc12458dc33a2e5e3bb0c3f95b2c5
vesoma <venkat.soma1@gmail.com>
2023-07-02 19:48:22 -0500

    updated routes

--- server.js
+++ server.js
@@ -11,10 +11,14 @@ const app = express();
 
 const port = process.env.PORT || 5000;
 
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "client", "build", "build/index.html"));
+});
+
+app.use(express.static(path.join(__dirname, "client", "build")));
+
 app.use(cors());
 app.use(express.json());
-const staticFilesDir = path.join(__dirname, "build");
-app.use(express.static(staticFilesDir));
 
 // app.get("*", (req, res) => {
 //   res.sendFile(path.join(staticFilesDir, "/"));
